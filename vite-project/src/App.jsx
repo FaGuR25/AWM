da5166e4f6f68c6d849185dc5c341d5d3fe3db90
Fatima <guadalupe20050@gmail.com>
2024-03-12 21:19:21 -0600

    Estilos CSS

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import '../src/styles.css';
 
 function App() {
 
@@ -8,55 +8,47 @@ function App() {
   const [resultado, setResultado] = useState(0);
 
   const handleChangeUno = (event) => {
-    console.log(event);
     setNumeroUno(event.target.value);
   }
 
-  const handleChangeDos = (e) => {
-    console.log(e);
-    setNumeroDos(e.target.value);
+  const handleChangeDos = (event) => {
+    setNumeroDos(event.target.value);
   }
 
-  const handleSumar = (event) => {
-     setResultado(parseFloat(numeroUno) + parseFloat(numeroDos));
+  const handleSumar = () => {
+    setResultado(parseFloat(numeroUno) + parseFloat(numeroDos));
   } 
 
-  const handleRestar = (event) => {
+  const handleRestar = () => {
     setResultado(parseFloat(numeroUno) - parseFloat(numeroDos));
- } 
+  } 
 
-  const handleMultiplicar = (event) => {
+  const handleMultiplicar = () => {
     setResultado(parseFloat(numeroUno) * parseFloat(numeroDos));
   } 
 
-  const handleDividir = (event) => {
+  const handleDividir = () => {
     setResultado(parseFloat(numeroUno) / parseFloat(numeroDos));
   } 
 
-  const nombre = "fatima";
   return (
- <>
-  {nombre}
-  <h1>CALCULADORA</h1>
-  <input type="text" placeholder="numero 1"  value={numeroUno} onChange={handleChangeUno}/>
-  <input type="text" placeholder="numero 2" value={numeroDos} onChange={handleChangeDos}/>
-  <hr />
-  <hr />
-  <button onClick={handleSumar}> sumar
-    </button>
-  <button onClick={handleRestar}> restar
-    </button>
-  <button onClick={handleMultiplicar}> multiplicar
-    </button>
-  <button onClick={handleDividir}> dividir
-  </button>
-  <hr />
-  <hr />
-  <p>resultado: { resultado } </p>
- 
- </> 
-  )
-  
+    <>
+      <h1 style={{ textAlign: 'center', fontFamily: 'monospace', fontSize: '40px' }}>CALCULADORA</h1>
+      <div style={{ textAlign: 'center' }}>
+        <input className="texto" type="text" placeholder="Numero 1" value={numeroUno} onChange={handleChangeUno} style={{ marginRight: '10px' }} />
+        <input className="texto" type="text" placeholder="Numero 2" value={numeroDos} onChange={handleChangeDos} />
+      </div>
+      <hr />
+      <div style={{ textAlign: 'center' }}>
+        <button className="button" onClick={handleSumar}>Sumar</button>
+        <button className="button" onClick={handleRestar}>Restar</button>
+        <button className="button" onClick={handleMultiplicar}>Multiplicar</button>
+        <button className="button" onClick={handleDividir}>Dividir</button>
+      </div>
+      <hr />
+      <p className="res">Resultado: {resultado}</p>
+    </>
+  );
 }
 
-export default App
+export default App;
